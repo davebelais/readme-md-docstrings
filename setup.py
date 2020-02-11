84313f14ca024af2b2f3aa87f5afcff654ac7993
David Belais <david.belais@nike.com>
2020-02-11 11:47:49 -0800

    Updated tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='readme-md-docstrings',
-    version="0.0.5",
+    version="0.0.6",
     description=(
         'Update README.md from Docstrings'
     ),
