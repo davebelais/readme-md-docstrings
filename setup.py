ae8900ea9fe134957ce883e8e6c1d6b7418b60be
David Belais <david.belais@nike.com>
2020-03-10 13:55:56 -0700

    Refactoring to accomodate PostgreSQL, updated docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='readme-md-docstrings',
-    version="0.0.8",
+    version="0.0.9",
     description=(
         'Update README.md from Docstrings'
     ),
